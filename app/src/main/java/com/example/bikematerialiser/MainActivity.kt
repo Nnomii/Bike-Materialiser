d4a328164e38f9439fedd88c8126ca588786f9dc
PC\Wojtek <W.P.Marczuk@student.tudelft.nl>
2023-04-23 17:30:26 +0200

    main gui design

--- app/src/main/java/com/example/bikematerialiser/MainActivity.kt
+++ app/src/main/java/com/example/bikematerialiser/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.bikematerialiser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textTitle: TextView = findViewById<TextView>(R.id.textTitle)
+        textTitle.animate().apply {
+            duration = 0
+            rotationYBy(10f)
+        }.start()
     }
 }
